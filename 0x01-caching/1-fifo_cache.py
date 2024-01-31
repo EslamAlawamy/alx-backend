18f2282968b00092855f3c8d7b13d75ba2ed68d7
Eslam Alawamy <eslamalawamy.com@gmail.com>
2024-01-31 17:08:03 +0000

    add: task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" FIFO caching module """
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache class """
+
+    def put(self, key, item):
+        """ to add an item in the cache """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if len(self.cache_data) > self.MAX_ITEMS:
+                discarded_key = sorted(self.cache_data.keys())[0]
+                del self.cache_data[discarded_key]
+                print(f"DISCARD: {discarded_key}")
+
+    def get(self, key):
+        """ to get an item by key """
+        return self.cache_data.get(key, None)
